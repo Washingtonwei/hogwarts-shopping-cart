06241b9291c290def8a2738498e4d53bd9d4f2fd
washingtonwei <weibingyang@gmail.com>
2024-04-10 20:51:53 -0500

    Reactivity system

--- src/App.vue
+++ src/App.vue
@@ -185,7 +185,7 @@
 import { ref } from 'vue'
 
 let username = 'Harry'
-let shoppingCartItems = [
+let shoppingCartItems = ref([
   {
     id: 1,
     productName: 'Dragon Liver',
@@ -226,7 +226,7 @@ let shoppingCartItems = [
     quantity: 1,
     image: 'src/assets/img/Nimbus2000.jpg'
   }
-]
+])
 
 let hideDetails = ref(false)
 </script>
