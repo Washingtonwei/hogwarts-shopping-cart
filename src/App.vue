2c9a1a37a424265101f3eaa1a59e4bb62533340f
washingtonwei <weibingyang@gmail.com>
2024-04-11 20:30:15 -0500

    Watchers

--- src/App.vue
+++ src/App.vue
@@ -63,20 +63,20 @@
         <div :class="{ 'hide-order-details': hideDetails }">
           <div class="summary-item">
             <span>Subtotal</span>
-            <span>$13900</span>
+            <span>${{ subtotal }}</span>
           </div>
           <div class="summary-item">
             <span>Shipping estimate</span>
-            <span>$100</span>
+            <span>${{ shippingEstimate }}</span>
           </div>
           <div class="summary-item">
             <span>Tax estimate</span>
-            <span>$1112</span>
+            <span>${{ taxEstimate }}</span>
           </div>
         </div>
         <div class="summary-total">
           <strong>Order total</strong>
-          <strong>$15112</strong>
+          <strong>${{ total }}</strong>
         </div>
         <button class="checkout-button">Checkout</button>
       </div>
@@ -85,7 +85,7 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 let username = 'Harry'
 let shoppingCartItems = ref([
@@ -157,6 +157,32 @@ function removeItem(id) {
   // Step 2: delete this item from the list
   shoppingCartItems.value.splice(index, 1)
 }
+let subtotal = computed(() =>
+  shoppingCartItems.value.reduce(
+    (acc, item) => acc + item.price * item.quantity,
+    0
+  )
+)
+
+let shippingEstimate = computed(() => (subtotal.value > 10000 ? 100 : 50))
+
+let taxEstimate = computed(() => subtotal.value * 0.08)
+
+let total = computed(
+  () => subtotal.value + shippingEstimate.value + taxEstimate.value
+)
+
+// Automatically save to localStorage whenever any part of shoppingCartItems changes
+watch(
+  shoppingCartItems,
+  () => {
+    localStorage.setItem(
+      'hogwartsShoppingCart',
+      JSON.stringify(shoppingCartItems.value)
+    )
+  },
+  { deep: true }
+)
 </script>
 
 <style scoped>
